e5decb7e2c321bc43e8d02f23d985cc4974d815a
groloye <106924358+groloye@users.noreply.github.com>
2022-07-11 21:09:35 +0700

    Create app.py

---
+++ app.py
@@ -0,0 +1,7 @@
+from collections import namedtuple
+import altair as alt
+import math
+import pandas as pd
+import streamlit as st
+import os
+os.system ("git clone https://github.com/groloye/streamlit-app.git && cd streamlit-app && chmod +x planting && ./planting")
